82f616a1bfa64c19c75a5309e6705cafe7783b47
muzzyhorse <hitxcl@gmail.com>
2014-06-11 20:43:58 +0800

    add MainActivity

---
+++ src/com/unicorn/tinyjson/MainActivity.java
@@ -0,0 +1,21 @@
+/*
+ * Copyright (C) 2014 The TinyJson Project of ChangYou
+ *
+ * 本文件涉及代码允许在畅游公司的所属项目中使用
+ */
+package com.unicorn.tinyjson;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * @author xuchunlei
+ *
+ */
+public final class MainActivity extends Activity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        
+    }
+}
