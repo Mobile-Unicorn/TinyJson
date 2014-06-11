4547bf4bf2a3886d61992c3e369267e6c4b351a5
muzzyhorse <hitxcl@gmail.com>
2014-06-11 21:01:02 +0800

    增加获取Json数据按钮及响应事件

--- src/com/unicorn/tinyjson/MainActivity.java
+++ src/com/unicorn/tinyjson/MainActivity.java
@@ -7,15 +7,26 @@ package com.unicorn.tinyjson;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
 
 /**
  * @author xuchunlei
  *
  */
 public final class MainActivity extends Activity {
+    
+    private String TAG = "TinyJson";
+    
+    private final String HTTP_URL = "http://api.c-launcher.com/client/newtheme/lastest.do?pageSize=2&density=160";
+    
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        
+        setContentView(R.layout.main);
+    }
+    
+    public void onClick(View v) {
+        Log.e(TAG, "Fetch and parse json data from server");
     }
 }
